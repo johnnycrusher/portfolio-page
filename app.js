e40cfdebe4114b983aa56184ef3c3d712bbc7ce3
JohnnyCrusher <John.huynh@student.qut.edu.au>
2018-01-18 20:12:46 +1000

    nav bar completed

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ var path = require('path');
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-
+app.use(express.static(__dirname + '/public'));
 app.listen(3000);
